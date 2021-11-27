aaa5b2de862876ed11dc8791b826ccdb2ccb1d3d
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2021-11-27 02:40:13 -0500

    m

--- Tablas/console.sql
+++ Tablas/console.sql
@@ -1,7 +1,7 @@
 set search_path = "public";
 
 -- PELICULAS
-CREATE TABLE Peliculas
+CREATE TABLE Pelicula
 (
     id              varchar(9),
     nombre          varchar(50),
@@ -11,3 +11,25 @@ CREATE TABLE Peliculas
     fecha_adquision date
 );
 
+CREATE TABLE Actor
+(
+    id              varchar(9),
+    nombre          varchar(50),
+);
+
+CREATE TABLE Director
+(
+    id              varchar(9),
+    nombre          varchar(50)
+);
+
+CREATE TABLE Genero
+(
+    id              int,
+    nombre          varchar(10),
+);
+CREATE TABLE Nivel_Publico
+(
+    id              int,
+    nombre          varchar(10),
+);
